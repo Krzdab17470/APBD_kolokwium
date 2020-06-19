b3f9673be34992a969d31f60a220fc1cffc11d57
Krzysztof Dąbkowski <s17470@pjwstk.edu.pl>
2020-06-19 21:06:32 +0200

    Push2

---
+++ APBD_Kolokwium/Controllers/MyController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APBD_Kolokwium.Controllers
+{
+    [ApiController]
+    [Route("api/application")]
+    
+    public class MyController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+
+
+    }
+}
